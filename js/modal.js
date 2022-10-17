ad206d13de8a891ca290bc157538ce5fbad13a4e
Ihor Bodianchuk <ihordobruy@gmail.com>
2022-10-17 14:34:40 +0300

    added 8hw

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+  const toggleModal = () =>
+   {
+    refs.modal.classList.toggle("is-hidden");
+  }
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+})();
